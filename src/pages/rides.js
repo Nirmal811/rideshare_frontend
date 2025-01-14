30926f82e9a07991d4f30952d57c860ed9f75eba
nirmal <me21b085@smail.iitm.ac.in>
2025-01-14 22:34:58 +0530

    navbar fixed

--- src/pages/rides.js
+++ src/pages/rides.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Logo from "../logo192.png";
 import Navbar from "../components/Navbar.js";
 
 export default function rides() {
   return (
     <div>
       <Navbar />
-      
     </div>
   )
 }
