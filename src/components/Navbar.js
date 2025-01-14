b35fb37189eba86319dc656e6918e5427d138cd5
nirmal <me21b085@smail.iitm.ac.in>
2025-01-14 22:32:04 +0530

    navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,73 @@
-import React from 'react'
+import React from 'react';
 import Logo from "../logo192.png";
 
 export default function Navbar() {
   return (
     <div>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <div class="container-fluid">
-        <img
-                className="img-thumbnail mx-auto d-block mb-2 my-2 me-2"
-                src={Logo}
-                width='40px'
-                alt="logo"
-            />
-          <a class="navbar-brand" href="/rides">
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div className="container-fluid">
+          <img
+            className="img-thumbnail mx-auto d-block mb-2 my-2 me-2"
+            src={Logo}
+            width="40px"
+            alt="logo"
+          />
+          <a className="navbar-brand" href="/rides">
             Ride Share
-            </a>
-          <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          </a>
+          <button
+            className="navbar-toggler collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="navbar-collapse collapse" id="navbarCollapse" >
-            <ul class="navbar-nav me-auto mb-2 mb-md-0">
-              <li class="nav-item mx-2">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <div className="navbar-collapse collapse" id="navbarCollapse">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item mx-2">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
               </li>
-              <li class="nav-item mx-2">
-                <a class="nav-link active" aria-current="page" href="/rides">Rides</a>
+              <li className="nav-item mx-2">
+                <a className="nav-link active" aria-current="page" href="/rides">
+                  Rides
+                </a>
               </li>
-              <li class="nav-item mx-2">
-                <a class="nav-link active" aria-current="page" href="/addride">Add Ride</a>
+              <li className="nav-item mx-2">
+                <a className="nav-link active" aria-current="page" href="/addride">
+                  Add Ride
+                </a>
               </li>
-              <li class="nav-item mx-2">
-                <a class="nav-link active" aria-current="page" href="/secrets">Secrets</a>
+              <li className="nav-item mx-2">
+                <a className="nav-link active" aria-current="page" href="/secrets">
+                  Secrets
+                </a>
               </li>
-              <li class="nav-item mx-2">
-                <a class="nav-link active" aria-current="register" href="/register">Register</a>
+              <li className="nav-item mx-2">
+                <a className="nav-link active" href="/register">
+                  Register
+                </a>
               </li>
             </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn btn-outline-success" type="submit">Search</button>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
             </form>
           </div>
         </div>
-      </nav>      
+      </nav>
     </div>
-  )
+  );
 }
